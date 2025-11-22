ccbee5dd6312a843937d18bf95a3135f5004fdc3
mayurhande90-maker <mayurhande90@gmail.com>
2025-11-22 21:34:35 +0530

    fix(icons): Update Projects icon SVG
    Replaced the SVG path for the Projects icon with a new, more refined design.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -101,7 +101,7 @@ export const NotesIcon: React.FC<IconProps> = (props) => (
 );
 
 export const ProjectsIcon: React.FC<IconProps> = (props) => (
-    <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 13.5h19.5m-19.5-3h19.5m-19.5-3h19.5M2.25 6.75h19.5M2.25 12a9.75 9.75 0 019.75-9.75v0a9.75 9.75 0 019.75 9.75v0a9.75 9.75 0 01-9.75 9.75v0A9.75 9.75 0 012.25 12v0z" />} />
+    <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />} />
 );
 
 export const DashboardIcon: React.FC<IconProps> = (props) => (
