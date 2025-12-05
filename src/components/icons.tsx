730683f9ecb46b1d911cbf35889fb0d370433292
mayurhande90-maker <mayurhande90@gmail.com>
2025-12-05 13:08:11 +0530

    feat: Redesign MagicPixa logo
    Updates the MagicPixa logo component to include a more dynamic and visually appealing design. The "Magic" part is now light, while "Pixa" uses a gradient text effect, enhancing brand recognition and aesthetic.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -411,11 +411,14 @@ export const PencilIcon: React.FC<IconProps> = (props) => (
 );
 
 export const MagicPixaLogo: React.FC<IconProps> = ({ className }) => (
-    <div className={`flex items-center gap-2 ${className}`}>
+    <div className={`flex items-center gap-2 font-sans ${className}`} style={{ fontFamily: "'Unbounded', sans-serif" }}>
         <div className="w-8 h-8 bg-gradient-to-br from-indigo-500 to-purple-600 rounded-lg flex items-center justify-center text-white font-bold text-xl">
             M
         </div>
-        <span className="font-bold text-xl tracking-tight text-[#1A1A1E]">MagicPixa</span>
+        <span className="font-bold text-xl tracking-tight">
+            <span className="font-light text-[#1A1A1E]">Magic</span>
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">Pixa</span>
+        </span>
     </div>
 );
 
@@ -481,7 +484,7 @@ export const GiftIcon: React.FC<IconProps> = (props) => (
 );
 
 export const CogIcon: React.FC<IconProps> = (props) => (
-    <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" /><path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />} />
+    <BaseIcon {...props} path={<><path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" /><path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></>} />
 );
 
 export const ChartBarIcon: React.FC<IconProps> = (props) => (
@@ -501,7 +504,7 @@ export const FlagIcon: React.FC<IconProps> = (props) => (
 );
 
 export const EyeIcon: React.FC<IconProps> = (props) => (
-    <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />} />
+    <BaseIcon {...props} path={<><path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></>} />
 );
 
 export const FilterIcon: React.FC<IconProps> = (props) => (
@@ -511,7 +514,7 @@ export const FilterIcon: React.FC<IconProps> = (props) => (
 // Badges
 export const BadgeNoviceIcon: React.FC<IconProps> = ({ className }) => (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-        <path fillRule="evenodd" d="M12 2.25a.75.75 0 01.75.75v.756a9.006 9.006 0 014.376 1.863l.53-.53a.75.75 0 011.06 1.06l-.53.53a9.005 9.005 0 011.863 4.376h.756a.75.75 0 010 1.5h-.756a9.005 9.005 0 01-1.863 4.376l.53.53a.75.75 0 01-1.06 1.06l-.53-.53a9.005 9.005 0 01-4.376 1.863v.756a.75.75 0 01-1.5 0v-.756a9.005 9.005 0 01-4.376-1.863l-.53.53a.75.75 0 01-1.06-1.06l.53-.53a9.005 9.005 0 01-1.863-4.376h-.756a.75.75 0 010-1.5h.756a9.005 9.005 0 011.863-4.376l-.53-.53a.75.75 0 011.06-1.06l.53.53a9.005 9.005 0 014.376-1.863V3a.75.75 0 01.75-.75zM6.75 12a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0z" clipRule="evenodd" />
+        <path fillRule="evenodd" d="M12 2.25a.75.75 0 01.75.75v.756a9.006 9.006 0 014.376 1.863l.53-.53a.75.75 0 011.06 1.06l-.53.53a9.005 9.005 0 011.863 4.376h.756a.75.75 0 010 1.5h-.756a9.005 9.005 0 01-1.863 4.376l.53.53a.75.75 0 01-1.06 1.06l-.53-.53a9.005 9.005 0 01-4.376 1.863v.756a.75.75 0 01-1.5 0v-.756a9.005 9.005 0 01-4.376-1.863l-.53.53a.75.75 0 01-1.06-1.06l.53.53a9.005 9.005 0 01-1.863-4.376h-.756a.75.75 0 010-1.5h.756a9.005 9.005 0 011.863-4.376l-.53-.53a.75.75 0 011.06-1.06l.53.53a9.005 9.005 0 014.376-1.863V3a.75.75 0 01.75-.75zM6.75 12a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0z" clipRule="evenodd" />
     </svg>
 );
 
