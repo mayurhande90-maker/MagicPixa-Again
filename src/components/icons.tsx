14a7ab56016ddfba5797c707590ad56a33d64d69
mayurhande90-maker <mayurhande90@gmail.com>
2025-12-15 23:29:38 +0530

    feat: Add brand icons module
    Introduces a new module for brand-specific icons and re-exports it from the main icons barrel file. This prepares for the integration of brand kit features.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,8 @@
+
 // This file is now a barrel file re-exporting icons from the modular structure.
 export * from './icons/types';
 export * from './icons/featureIcons';
 export * from './icons/uiIcons';
 export * from './icons/actionIcons';
 export * from './icons/statusIcons';
+export * from './icons/brandIcons';
