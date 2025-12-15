65bee583cc369dbec5c0008301949c9479fd1040
mayurhande90-maker <mayurhande90@gmail.com>
2025-12-15 23:19:08 +0530

    feat: Add CaptionIcon and re-export HeadshotIcon
    Introduces a new `CaptionIcon` component and re-exports `PixaHeadshotIcon` from `featureIcons.tsx`. This change standardizes icon component exports and ensures `PixaHeadshotIcon` is correctly made available.

--- src/components/icons/uiIcons.tsx
+++ src/components/icons/uiIcons.tsx
@@ -68,7 +68,7 @@ export const DashboardIcon: React.FC<IconProps> = ({ className }) => (
     </svg>
 );
 
-// Custom Landscape Picture Icon (Replaced with new My Creations SVG)
+// Custom Landscape Picture Icon
 export const ProjectsIcon: React.FC<IconProps> = ({ className }) => (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
         <g fill="none">
@@ -310,7 +310,7 @@ export const PhoneIcon: React.FC<IconProps> = (props) => (
     <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />} />
 );
 
-// Renamed from ProductStudioIcon
+// Renamed from ProductStudioIcon to BrandKitIcon, used for product studio alias as well
 export const BrandKitIcon: React.FC<IconProps> = ({ className }) => (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
         <path fill="#424242" d="M27 7h-6c-1.7 0-3 1.3-3 3v3h2v-3c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v3h2v-3c0-1.7-1.3-3-3-3z"/>
@@ -322,11 +322,3 @@ export const BrandKitIcon: React.FC<IconProps> = ({ className }) => (
 
 // Kept for backward compatibility if needed, but now aliased
 export const ProductStudioIcon = BrandKitIcon;
-
-export const ChatBubbleLeftIcon: React.FC<IconProps> = (props) => (
-    <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />} />
-);
-
-export const PaperAirplaneIcon: React.FC<IconProps> = (props) => (
-    <BaseIcon {...props} path={<path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />} />
-);
