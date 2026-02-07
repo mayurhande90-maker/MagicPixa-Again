6cfb55953adce15e5512e746c328b33c1a8fbd42
mayurhande90-maker <mayurhande90@gmail.com>
2026-02-07 20:57:21 +0530

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,2 +1,9 @@
-// This file is not used.
-// The main and complete icon set is located at /src/components/icons.tsx.
+// This file is now a barrel file re-exporting icons from the modular structure.
+export * from './icons/types';
+export * from './icons/featureIcons';
+export * from './icons/uiIcons';
+export * from './icons/actionIcons';
+export * from './icons/statusIcons';
+export * from './icons/brandIcons';
+export * from './icons/adMakerIcons';
+export * from './icons/headshotIcons';
