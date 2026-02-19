bd1a6be938616852d6d716d6874638a7e2f091b8
mayurhande90-maker <mayurhande90@gmail.com>
2026-02-19 15:23:29 +0530

    feat: Add Fluent Home icon for mobile navigation
    Introduces a new Fluent Home icon component to be used in the mobile bottom navigation bar. This enhances the visual consistency and user experience of the mobile interface.

--- src/mobile/components/MobileBottomNav.tsx
+++ src/mobile/components/MobileBottomNav.tsx
@@ -8,6 +8,28 @@ interface MobileBottomNavProps {
     setActiveTab: (tab: View) => void;
 }
 
+const FluentHomeIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg className={className} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill="url(#fluentColorHome280)" d="M10 15h8v10h-8z"/>
+        <path fill="url(#fluentColorHome281)" d="M15.408 4.498a2.25 2.25 0 0 0-2.816 0l-7.75 6.217A2.25 2.25 0 0 0 4 12.47v10.28A2.25 2.25 0 0 0 6.25 25H11v-7.75c0-.69.56-1.25 1.25-1.25h3.5c.69 0 1.25.56 1.25 1.25V25h4.75A2.25 2.25 0 0 0 24 22.75V12.47a2.25 2.25 0 0 0-.842-1.755z"/>
+        <path fill="url(#fluentColorHome282)" fill-rule="evenodd" d="m14 5.715l-9.714 7.863a1.25 1.25 0 0 1-1.573-1.943L12.9 3.39a1.75 1.75 0 0 1 2.202 0l10.186 8.245a1.25 1.25 0 0 1-1.573 1.943z" clip-rule="evenodd"/>
+        <defs>
+            <linearGradient id="fluentColorHome280" x1="14" x2="7.592" y1="15" y2="26.396" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#944600"/>
+                <stop offset="1" stop-color="#CD8E02"/>
+            </linearGradient>
+            <linearGradient id="fluentColorHome281" x1="5.909" x2="26.285" y1="2.976" y2="18.752" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FFD394"/>
+                <stop offset="1" stop-color="#FFB357"/>
+            </linearGradient>
+            <linearGradient id="fluentColorHome282" x1="10.203" x2="14.689" y1=".421" y2="13.551" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FF921F"/>
+                <stop offset="1" stop-color="#EB4824"/>
+            </linearGradient>
+        </defs>
+    </svg>
+);
+
 const FluentProfileIcon: React.FC<{ className?: string }> = ({ className }) => (
     <svg className={className} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill="url(#fluentColorPerson280)" d="M21 16a3 3 0 0 1 3 3v.715C24 23.292 19.79 26 14 26S4 23.433 4 19.715V19a3 3 0 0 1 3-3z"/>
@@ -32,7 +54,7 @@ const FluentProfileIcon: React.FC<{ className?: string }> = ({ className }) => (
 
 export const MobileBottomNav: React.FC<MobileBottomNavProps> = ({ activeTab, setActiveTab }) => {
     const navItems = [
-        { id: 'home_dashboard', label: 'Home', icon: HomeIcon },
+        { id: 'home_dashboard', label: 'Home', icon: FluentHomeIcon },
         { id: 'dashboard', label: 'Features', icon: DashboardIcon },
         { id: 'creations', label: 'Projects', icon: ProjectsIcon },
         { id: 'profile', label: 'Profile', icon: FluentProfileIcon },
